0a58e2506dbd6d0046b540345ece7dc0f3c2d501
mark_march <markofmarch@gmail.com>
2011-10-06 11:48:58 -0400

    removed output and ast elements

--- src/main/scala/edu/nyu/compiler/TackParser.scala
+++ src/main/scala/edu/nyu/compiler/TackParser.scala
@@ -3,32 +3,41 @@ package edu.nyu.compiler
 import scala.util.parsing.combinator.{PackratParsers, ImplicitConversions}
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
+/**
+ * A simple parser that accepts programs written in Tack, a statically typed
+ * language invented for the Compiler Construction course at NYU.
+ *
+ * This parser uses the parser combinator provided in the Scala library, and only
+ * parse the input but does not generate AST.
+ */
 trait TackParser extends StandardTokenParsers with PackratParsers with ImplicitConversions {
-  type PAny = PackratParser[Any]
   // replace the default lexical with TackLexical
   override val lexical = new TackLexical
 
+  // add delimiters
   lexical.delimiters ++= TackParser.delimiters
 
+  // add keywords
   lexical.reserved ++= TackParser.reserved
 
+  // productions
   def program = (funDef +)
 
   def funDef = ident ~ "=" ~ "fun" ~ funType ~ blockStmt
 
-  def _type: Parser[Any] = arrayType | recordType | "bool" | "int" | "string"
+  def typ: Parser[Any] = arrayType | recordType | "bool" | "int" | "string"
 
-  def arrayType = "[" ~ _type ~ "]"
+  def arrayType = "[" ~ typ ~ "]"
 
   def recordType = "(" ~ repsep(fieldType, ",") ~ ")"
 
-  def fieldType = ident ~ ":" ~ _type
+  def fieldType = ident ~ ":" ~ typ
 
   def funType = recordType ~ "->" ~ returnType
 
-  def returnType = _type | "void"
+  def returnType = typ | "void"
 
-  def stmt: Parser[Any] = varDef | assignStmt | blockStmt | callStmt | forStmt | ifStmt | returnStmt | whileStmt
+  def stmt : Parser[Any] = varDef | assignStmt | blockStmt | callStmt | forStmt | ifStmt | returnStmt | whileStmt
 
   def varDef = ident ~ "=" ~ expr ~ ";"
 
@@ -36,9 +45,7 @@ trait TackParser extends StandardTokenParsers with PackratParsers with ImplicitC
 
   def blockStmt = "{" ~> rep(stmt) <~ "}"
 
-  def callStmt = Expressions.callExpr ~ ";" ^^ {
-    case a => println("call stmt"); a
-  }
+  def callStmt = Expressions.callExpr ~ ";"
 
   def forStmt = "for" ~ ident ~ "in" ~ expr ~ blockStmt
 
@@ -48,36 +55,38 @@ trait TackParser extends StandardTokenParsers with PackratParsers with ImplicitC
 
   def whileStmt = "while" ~ expr ~ blockStmt
 
+  // wrap expressions into an object, expressionN, where the lower N the higher
+  // precedence
   object Expressions {
-    // () and literals
-    lazy val expr1: PackratParser[Any] = (ident | literal | "(" ~> expr9 <~ ")") ^^ {
-      case a => println("expr1 " + a.toString); a
-    }
+    // parentheses and literals
+    lazy val expr1: PackratParser[Any] = (ident | literal | "(" ~> expr9 <~ ")")
 
     // postfix call, cast , field and subscript
-    lazy val callExpr = expr1 ~ ("(" ~> repsep(expr9, ",") <~ ")") ^^ {
-      case a => println("call expr"); a
-    }
-    lazy val castExpr = expr2 ~ (":" ~> _type) ^^ {
-      case a => println("cast"); a
-    }
+    lazy val callExpr = expr1 ~ ("(" ~> repsep(expr9, ",") <~ ")")
+    lazy val castExpr = expr2 ~ (":" ~> typ)
     lazy val fieldExpr = expr2 ~ "." ~> ident
     lazy val subscriptExpr = expr2 ~ ("[" ~> expr9 <~ "]")
     lazy val expr2: PackratParser[Any] = callExpr | castExpr | subscriptExpr | fieldExpr | expr1
 
+    // prefix operator ! and -
     lazy val expr3: PackratParser[Any] = {
       lazy val not = "!" ~> expr3
       lazy val minus = "-" ~> expr3
       not | minus | expr2
     }
 
-    def makeParser(eqPrecdence: PackratParser[Any], higherPrecdence: PackratParser[Any])(ops: String*) = {
+    // create a parser using an expression with equal precedence and another expression
+    // with higher precedence
+    def makeParser(eqPrecedence: PackratParser[Any], higherPrecedence: PackratParser[Any])(ops: String*) = {
+      // create a parser for each operators in the list
       val opsParsers = ops.toList map {
-        (str) => eqPrecdence <~ str ~ higherPrecdence
+        (str) => eqPrecedence <~ str ~ higherPrecedence
       }
-      opsParsers.reduceLeft(_ | _) | higherPrecdence
+      // combine those parsers to form alternatives
+      opsParsers.reduceLeft(_ | _) | higherPrecedence
     }
 
+    // expressions with difference precedence
     lazy val expr4: PackratParser[Any] = makeParser(expr4, expr3)("*", "/", "%")
     lazy val expr5: PackratParser[Any] = makeParser(expr5, expr4)("+", "-")
     lazy val expr6: PackratParser[Any] = makeParser(expr6, expr5)("<=", "<", ">=", ">")
@@ -108,28 +117,4 @@ object TackParser {
     "+", "*", "/", "%", "_", "|", "&", "-", "=", ":", ">", "<",
     ",", ";", ".", ":", "#", "!")
 }
-
-sealed abstract class Expression
-
-case class CallExpr(expr: Expression, rest: Expression*)
-
-case class Literal(lit: String) extends Expression
-
-case class InfixOp(op: String, left: Expression, right: Expression) extends Expression
-
-case class UnaryOp(op: String, operand: Expression) extends Expression
-
-case class PrefixOp(op: String, operand: Expression) extends Expression
-
-sealed abstract class Statement
-
-case class VarDef(id: String, expr: Expression) extends Statement
-
-case class AssignStmt(left: Expression, right: Expression) extends Statement
-
-case class BlockStmt(list: List[Statement]) extends Statement
-
-case class CallStmt(expr: CallExpr) extends Statement
-
-case class ForStmt(id: String, expr: Expression, stmt: BlockStmt) extends Statement
 
