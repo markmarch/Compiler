328731557cca926c34cb89f57be317037581bfd7
mark_march <markofmarch@gmail.com>
2011-10-06 11:49:57 -0400

    added some comments

--- src/main/scala/edu/nyu/compiler/TackLexical.scala
+++ src/main/scala/edu/nyu/compiler/TackLexical.scala
@@ -3,23 +3,19 @@ package edu.nyu.compiler
 import util.parsing.combinator.lexical.StdLexical
 import scala.util.parsing.input.CharArrayReader.EofCh
 
-/**
- * Created by IntelliJ IDEA.
- * User: xiaomingjia
- * Date: 10/5/11
- * Time: 10:37 AM
- * To change this template use File | Settings | File Templates.
- */
-
 class TackLexical extends StdLexical{
+  // added comments to whitespace to be ignored
   override def whitespace : Parser[Any] = rep (
       whitespaceChar
     | '#' ~ rep(chrExcept(EofCh, '\n'))
     | '#' ~ failure("unclosed comment"))
 
+  // string literal parser
   def string : Parser[Token] = '\"' ~ rep(('\\' ~ chrExcept('\n',EofCh))
     | chrExcept('\"','\n', EofCh)) ~ '\"' ^^ { case '\"' ~ chars ~ '\"' => StringLit(chars mkString "")}
 
+  // override the default token parser to accept string literal
+  // and ignore single quotes string literals
   override def token : Parser[Token] =
     ( identChar ~ rep( identChar | digit )              ^^ { case first ~ rest => processIdent(first :: rest mkString "") }
     | digit ~ rep( digit )                              ^^ { case first ~ rest => NumericLit(first :: rest mkString "") }
