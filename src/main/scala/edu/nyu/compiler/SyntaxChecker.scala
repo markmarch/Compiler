862b62ba9f31bd067092117d4887b15d8ce9c0ad
mark_march <markofmarch@gmail.com>
2011-10-06 11:49:42 -0400

    Modified main method to produce the desired output for the assignment

--- src/main/scala/edu/nyu/compiler/SyntaxChecker.scala
+++ src/main/scala/edu/nyu/compiler/SyntaxChecker.scala
@@ -2,32 +2,23 @@ package edu.nyu.compiler
 
 import scala.io.Source
 import java.io.FileNotFoundException
-import scala.util.parsing.combinator.PackratParsers
 
 /**
- * Created by IntelliJ IDEA.
- * User: xiaomingjia
- * Date: 10/5/11
- * Time: 8:54 PM
- * To change this template use File | Settings | File Templates.
+ * Syntax checker that takes a file name as argument.
+ * Will print "syntax error" if any syntax error is found,
+ * otherwise no output.
  */
-
-object SyntaxChecker extends TackParser{
+object SyntaxChecker extends TackParser {
   def main(args: Array[String]): Unit = {
-
-
-    def addZero(n: Int) = "0" * (3 - n.toString.length) + n.toString
-
-    val files = ((1 to 20).map(addZero(_))).toList ::: List("022","023","024")
-    for (file <- files) {
-      try {
-        println(file)
-        val s = Source.fromFile("test/" + file + ".tack").getLines.reduceLeft(_ + "\n" + _)
-        val tokens = new PackratReader(new lexical.Scanner(s))
-        println(phrase(program)(tokens))
-      } catch {
-        case e: FileNotFoundException =>
+    try {
+      val s = Source.fromFile(args(0)).getLines.reduceLeft(_ + "\n" + _)
+      val tokens = new PackratReader(new lexical.Scanner(s))
+      phrase(program)(tokens) match{
+        case Success(_,_) =>
+        case e : NoSuccess => println("Syntax error : " + e.msg)
       }
+    } catch {
+      case e: FileNotFoundException => println(e.getMessage)
     }
   }
 }
