11af1a21344db0632125355e19ba9cfea6d04c4a
mark_march <markofmarch@gmail.com>
2011-10-19 18:51:33 -0400

    Test suite for ASTPrinter

---
+++ src/test/scala/edu/nyu/compiler/ASTPrinterSuite.scala
@@ -0,0 +1,16 @@
+package edu.nyu.compiler
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: xiaomingjia
+ * Date: 10/19/11
+ * Time: 5:45 PM
+ */
+
+class ASTPrinterSuite extends FunSuite {
+
+  test("print 001.tack") {
+  }
+}
