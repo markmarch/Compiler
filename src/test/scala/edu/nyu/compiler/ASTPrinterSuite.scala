0fc47700ba8d71946c22b87c00e947598b6002b3
mark_march <markofmarch@gmail.com>
2011-10-24 15:56:23 -0400

    Test suite for ASTPrinter

--- src/test/scala/edu/nyu/compiler/ASTPrinterSuite.scala
+++ src/test/scala/edu/nyu/compiler/ASTPrinterSuite.scala
@@ -1,16 +1,29 @@
 package edu.nyu.compiler
 
 import org.scalatest.FunSuite
+import java.io.File
+import io.Source
+import edu.nyu.compiler.ASTPrinter.TackFileFilter
 
-/**
- * Created by IntelliJ IDEA.
- * User: xiaomingjia
- * Date: 10/19/11
- * Time: 5:45 PM
- */
+class ASTPrinterSuite extends FunSuite{
+  test("printAst") {
+    val path = "test/resources/pr2-test"
+    val fileNames = (1 to 20).map((number) => "0" * (3 - number.toString.length) + number)
+    for{
+      name <- fileNames ; 
+      val srcFile = new File(path + "/" + name + ".tack")
+      if srcFile.exists()
+    } {
+      check(srcFile, new File(path + "/" + name + ".ast"))
+    }
+  }
 
-class ASTPrinterSuite extends FunSuite {
 
-  test("print 001.tack") {
+  def check(srcFile : File, correctAstFile : File) = {
+    println("checking " + srcFile.getName)
+    val result = ASTPrinter.getAst(srcFile).split("\n").toList
+    val expectedLines = Source.fromFile(correctAstFile).getLines().filter(_.length > 0).toList
+    val comparsion = expectedLines.zip(result)
+    comparsion.foreach((pair) => assert(pair._1 === pair._2))
   }
 }
