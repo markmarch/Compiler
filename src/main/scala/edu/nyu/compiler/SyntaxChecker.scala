77989f4031bcf29878e4fcb685492b04257d5823
mark_march <markofmarch@gmail.com>
2011-10-24 15:54:45 -0400

    Extract method check and commented out main method.

--- src/main/scala/edu/nyu/compiler/SyntaxChecker.scala
+++ src/main/scala/edu/nyu/compiler/SyntaxChecker.scala
@@ -1,7 +1,7 @@
 package edu.nyu.compiler
 
 import scala.io.Source
-import java.io.FileNotFoundException
+import java.io.{File, FileNotFoundException}
 
 /**
  * Syntax checker that takes a file name as argument.
@@ -9,13 +9,19 @@ import java.io.FileNotFoundException
  * otherwise no output.
  */
 object SyntaxChecker extends TackParser {
-  def main(args: Array[String]): Unit = {
+//  def main(args: Array[String]) = {
+//    val dir = new File("test/resources/pr1-test")
+//    dir.listFiles().foreach((f) => check(f))
+//  }
+
+  def check(file: File) = {
     try {
-      val s = Source.fromFile(args(0)).getLines.reduceLeft(_ + "\n" + _)
+      println(file.getName)
+      val s = Source.fromFile(file).getLines().reduceLeft(_ + "\n" + _)
       val tokens = new PackratReader(new lexical.Scanner(s))
-      phrase(program)(tokens) match{
-        case Success(_,_) =>
-        case e : NoSuccess => println("Syntax error : " + e.msg)
+      phrase(program)(tokens) match {
+        case Success(_, _) =>
+        case e: NoSuccess => println("Syntax error : " + e.msg)
       }
     } catch {
       case e: FileNotFoundException => println(e.getMessage)
