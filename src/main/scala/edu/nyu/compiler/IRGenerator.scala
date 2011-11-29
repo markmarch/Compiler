b4c369ef25e97c1ed62bdda19633fa6712688c28
mark_march <markofmarch@gmail.com>
2011-11-28 22:19:19 -0500

    Changed how IR is generated.
    Walk though the AST tree and store generated code in art node.

--- src/main/scala/edu/nyu/compiler/IRGenerator.scala
+++ src/main/scala/edu/nyu/compiler/IRGenerator.scala
@@ -9,10 +9,11 @@ import collection.mutable.ListBuffer
  * Time: 3:59 PM
  */
 
-trait IRGenerator extends TypeAnalyzer{
+trait IRGenerator extends TypeAnalyzer {
   private var nextLabelIndex = 0
   private var nextTempIndex = 0
   private val relationOp = Set("==", "!=", ">=", ">", "<=", "<")
+  private val arithmeticOp = Set("+", "-", "*", "/", "%")
 
   def mkLabel() = {
     nextLabelIndex += 1
@@ -26,226 +27,296 @@ trait IRGenerator extends TypeAnalyzer{
 
   def generateIR(p: Program): List[String] = {
     analyzeType(p) match {
-      case Left(e) => e.foreach(println); Nil
+      case Left(e) => println(e); Nil
       case Right(_) => gen(p)
     }
   }
 
-  def gen(node: AstNode): List[String] = node match {
-    case p: Program => p.funList.flatMap(gen)
-    case f: FunDef =>
-      symbolTable.push(f.scope, false)
-      val code = genFunDef(f.id.name, f.typ, f.blockStmt.stmtList.flatMap(gen))
-      symbolTable.pop()
-      code
-    case s: Stmt => genStmt(s)
+  def gen(p: Program): List[String] = p.funList.flatMap(genFunDef)
+
+  def genFunDef(f: FunDef) = {
+    genStmt(f.blockStmt)
+    f.id.name + " = fun" + f.typ.fromType.toString + " -> " + f.typ.returnType.toString :: f.blockStmt.code.map("  " + _)
   }
 
-  def genFunDef(id: String, typ: FunType, stmt: List[String]) =
-    id + " = fun" + typ.fromType.toString + " -> " + typ.returnType.toString :: stmt.map("  " + _)
-
-  def genStmt(stmt: Stmt): List[String] = stmt match {
-    case VarDef(VarId(id), expr) => genVarDef(id, expr)
-    case AssignStmt(left, right) => genAssignment(left, right)
-    case BlockStmt(stmtList) =>
-      symbolTable.push(stmt.scope, false)
-      val code = stmtList.flatMap(genStmt)
-      symbolTable.pop()
-      code
-    case CallStmt(callExpr) => genCallExpr(callExpr.callee, callExpr.paramList)._2
-    case ForStmt(varId, expr, bs) =>
-      symbolTable.push(stmt.scope, false)
-      val code = genForStmt(varId.name, expr, bs)
-      symbolTable.pop()
-      code
-    case IfStmt(expr, bs, optBs) => genIfStmt(expr, bs, optBs)
-    case ReturnStmt(optExpr) => genReturnStmt(optExpr)
+  def genStmt(stmt: Stmt): Unit = stmt match {
+    case v: VarDef => genVarDef(v)
+    case a: AssignStmt => genAssignStmt(a)
+    case b: BlockStmt => genBlockStmt(b)
+    case c: CallStmt => genCallStmt(c)
+    case f: ForStmt => genForStmt(f)
+    case i: IfStmt => genIfStmt(i)
+    case r: ReturnStmt => genReturnStmt(r)
+    case w: WhileStmt => genWhileStmt(w)
   }
 
-  def genIfStmt(expr: Expression, bs: BlockStmt, optBs: Option[BlockStmt]) = {
-    val trueLabel = mkLabel()
-    val (jumpingCode, c) = expr match {
-      case InfixExpr(op, left, right) if relationOp.contains(op) =>
-        val (a1, c1) = genExpr(left)
-        val (a2, c2) = genExpr(right)
-        ("if " + a1 + " " + op + " " + a2 + " goto " + trueLabel + ";", c1 ::: c2)
-      case PrefixExpr("!", expr) =>
-        val (a, c) = genExpr(expr)
-        ("ifFalse " + a + " goto " + trueLabel + ";", c)
+  def genCallStmt(c: CallStmt) {
+    genCallExpr(c.expr)
+    c.code = c.expr.code ::: List(c.expr.address)
+  }
+
+  def genBlockStmt(b: BlockStmt) {
+    b.stmtList.foreach(genStmt)
+    b.code = b.stmtList.flatMap(_.code)
+  }
+
+  def genIfStmt(i: IfStmt) {
+    i.expr.t = mkLabel()
+    i.expr.f = mkLabel()
+    genJumpingCode(i.expr)
+    genStmt(i.blockStmt)
+
+    val code = new ListBuffer[String]
+    code.appendAll(i.expr.code)
+    code.append(i.expr.t + ":")
+    code.appendAll(i.blockStmt.code)
+    code.append(i.expr.f + ":");
+    i.optBlockStmt match {
+      case Some(b) => genStmt(b); code.appendAll(b.code)
+      case None =>
+    }
+    i.code = code.toList
+  }
+
+  def genReturnStmt(r: ReturnStmt) {
+    r.optExpr match {
+      case Some(e) =>
+        genValueCode(e)
+        r.code = e.code ::: List("return " + e.address + ";")
+      case None => r.code = List("return" + ";")
+    }
+  }
+
+  def genWhileStmt(w: WhileStmt) {
+    w.before = mkLabel()
+    w.after = mkLabel()
+    w.expr.t = mkLabel()
+    w.expr.f = w.after
+    genJumpingCode(w.expr)
+    genStmt(w.blockStmt)
+    val code = new ListBuffer[String]
+    code.append(w.before + ":")
+    code.appendAll(w.expr.code)
+    code.append(w.expr.t)
+    code.appendAll(w.blockStmt.code)
+    code.append("goto " + w.before)
+    code.append(w.after + ":")
+  }
+
+  def genVarDef(v: VarDef) {
+    genAssignment(v, v.varId, v.expr)
+  }
+
+  def genAssignment(s: Stmt, from: Expression, to: Expression) {
+    genValueCode(from)
+
+    to match {
+      case InfixExpr(op, left, right) if arithmeticOp.contains(op) =>
+        genValueCode(left)
+        genValueCode(right)
+        s.code = from.code ::: left.code :::
+          right.code ::: List(from.address + " = " + left.address + " " + op + " " + right.address + ";")
+      case PrefixExpr(op, e) =>
+        genValueCode(e)
+        s.code = from.code ::: e.code :::
+          List(from.address + " = " + op + e.address)
       case _ =>
-        val (a, c) = genExpr(expr)
-        ("if " + a + " goto " + trueLabel + ";", c)
+        genValueCode(to)
+        s.code = from.code ::: to.code ::: List(from.address + " = " + to.address)
     }
-    val falseLabel = mkLabel()
+  }
+
+  def genAssignStmt(a: AssignStmt) {
+    genAssignment(a, a.left, a.right)
+  }
+
+  def genForStmt(f: ForStmt) = {
+    genValueCode(f.expr)
     val code = new ListBuffer[String]
-    code.appendAll(c)
-    code.append(jumpingCode)
-    code.append("goto " + falseLabel + ";")
-    code.appendAll(trueLabel + ":" :: bs.stmtList.flatMap(genStmt))
-
-    val optionCode = optBs match {
-      case Some(s) => s.stmtList.flatMap(genStmt)
-      case None => Nil
-    }
-
-    code.appendAll(falseLabel + ":" :: optionCode)
-    code.toList
-  }
-
-  def genReturnStmt(optExpr: Option[Expression]): List[String] = optExpr match {
-    case Some(e) =>
-      val (a, c) = genExpr(e)
-      c ::: List("return " + a + ";")
-    case None => List("return" + ";")
-  }
-
-  def genWhileStmt(expr: Expression, bs: BlockStmt) = {
-    val code = new ListBuffer[String]
-    val begin = mkLabel()
-    val after = mkLabel()
-    val (a, c) = genExpr(expr)
-    code.append(begin + ":")
-    code.appendAll(c)
-    code.append("ifFalse " + a + " goto " + after + ";")
-    symbolTable.push(bs.scope)
-    code.appendAll(bs.stmtList.flatMap(genStmt))
-    symbolTable.pop()
-    code.append("goto " + begin + ";")
-    code.append(after + ":")
-    code.toList
-  }
-
-  def genVarDef(id: String, expr: Expression) = {
-    val symbol = symbolTable.lookup(id)
-    val (address, code) = expr match {
-      case RecordLit(fieldLitList) =>
-        val typ = symbol.get.typ.asInstanceOf[RecordType]
-        genRecordLit(typ, fieldLitList)
-      case ArrayLit(exprList) =>
-        val typ = symbol.get.typ.asInstanceOf[ArrayType]
-        genArrayLit(typ, exprList)
-      case _ => genExpr(expr)
-    }
-    code ::: List(id + " = " + address + ";")
-  }
-
-
-  def genAssignment(left: Expression, right: Expression): List[String] = {
-    val (a1, c1) = genExpr(left)
-    val (a2, c2) = genExpr(right)
-    c1 ::: c2 ::: List(a1 + " = " + a2 + ";")
-  }
-
-  def genForStmt(id: String, expr: Expression, bs: BlockStmt) = {
-    val code = new ListBuffer[String]
-    val (a, c) = genExpr(expr)
-    code.appendAll(c)
+    code.appendAll(f.expr.code)
     val size = mkTemp()
     val index = mkTemp()
-    code.append("param[0 : 1] = " + a + ";")
+    code.append("param[0 : 1] = " + f.expr.address + ";")
     code.append(size + " = call size : 1" + ";")
     code.append(index + " = 0;")
     val begin = mkLabel()
     val after = mkLabel()
     code.append(begin)
     code.append("if " + index + " >= " + size + " goto " + after + ";")
-    code.append(id + " = " + a + "[" + index + "]" + ";")
-    code.appendAll(bs.stmtList.flatMap(genStmt))
+    code.append(f.varId.name + " = " + f.expr.address + "[" + index + "]" + ";")
+    f.blockStmt.stmtList.foreach(genStmt)
+    code.appendAll(f.blockStmt.stmtList.flatMap(_.code))
     code.append("goto " + begin + ";")
     code.append(after + ":")
-    code.toList
+    f.code = code.toList
   }
 
-  def genExpr(expr: Expression): (String, List[String]) = expr match {
-    case id: VarId => (id.name, Nil)
-    case b : BoolLit => (b.value.toString, Nil)
-    case s : StringLit => ("\"" + s.value + "\"", Nil)
-    case i : IntLit => (i.value.toString, Nil)
-    case n : NullLit => ("null", Nil)
-    case c: CallExpr => genCallExpr(c.callee, c.paramList)
-    case c: CastExpr => genCastExpr(c.expr, c.typ)
-    case f: FieldExpr => genFieldExpr(f.expr, f.fieldId.name)
-    case p: ParenExpr => genExpr(p.expr)
-    case p: PrefixExpr => genPrefixExpr(p.op, p.expr)
-    case i: InfixExpr => genInfixExpr(i.op, i.left,  i.right)
-    case s: SubscriptExpr => genSubscriptExpr(s.left, s.right)
+  def genJumpingCode(expr: Expression): Unit = {
+    expr match {
+      case InfixExpr("||", left, right) =>
+        left.t = expr.t
+        left.f = mkLabel()
+        right.t = expr.t
+        right.f = expr.f
+        genJumpingCode(left)
+        genJumpingCode(right)
+        expr.code = left.code ::: List(left.f + ":") ::: right.code
+      case InfixExpr("&&", left, right) =>
+        left.t = mkLabel()
+        left.f = expr.f
+        right.t = expr.t
+        right.f = expr.f
+        genJumpingCode(left)
+        genJumpingCode(right)
+        expr.code = left.code ::: List(left.t + ":") ::: right.code
+      case InfixExpr(op, left, right) if relationOp.contains(op) =>
+        genValueCode(left)
+        genValueCode(right)
+        expr.code = left.code ::: right.code :::
+          List("if " + left.address + " " + op + " " + right.address + " goto " + expr.t + ";",
+            "goto " + expr.f + ";")
+      case PrefixExpr("!", e) =>
+        e.t = expr.f
+        e.f = expr.t
+        genJumpingCode(e)
+        expr.code = e.code
+      case BoolLit(true) =>
+        expr.code = List("goto " + expr.t + ";")
+      case BoolLit(false) =>
+        expr.code = List("goto " + expr.f + ";")
+    }
   }
 
-  def genSubscriptExpr(left : Expression, right : Expression) : (String, List[String]) = {
-    val (a1, c1) = genExpr(left)
-    val (a2, c2) = genExpr(right)
-    val temp = mkTemp()
-    (temp, c1 ::: c2 ::: List(temp + " = " + a1 + "[" + a2 + "];"))
+  def genValueCode(expr: Expression): Unit = expr match {
+    case i: VarId => i.address = i.name
+    case b: BoolLit => b.address = b.value.toString
+    case s: StringLit => s.address = "\"" + s.value + "\""
+    case i: IntLit => i.address = i.value.toString
+    case n: NullLit => n.address = "null"
+    case a: ArrayLit => genArrayLit(a)
+    case r: RecordLit => genRecordLit(r)
+    case f: FieldLit => genFieldLit(f)
+    case c: CallExpr => genCallExpr(c)
+    case c: CastExpr => genCastExpr(c)
+    case f: FieldExpr => genFieldExpr(f)
+    case p: ParenExpr => genParenExpr(p)
+    case p: PrefixExpr => genPrefixExpr(p)
+    case i: InfixExpr => genInfixExpr(i)
+    case s: SubscriptExpr => genSubscriptExpr(s)
   }
 
-  def genArrayLit(typ: ArrayType, exprList: List[Expression]): (String, List[String]) = {
+  def genParenExpr(p: ParenExpr) {
+    genValueCode(p.expr)
+    p.address = p.expr.address
+    p.code = p.expr.code
+  }
+
+  def genSubscriptExpr(subExpr: SubscriptExpr) {
+    genValueCode(subExpr.left)
+    genValueCode(subExpr.right)
+    subExpr.address = subExpr.left.address + "[" + subExpr.right.address + "]"
+    subExpr.code = subExpr.left.code ::: subExpr.right.code
+  }
+
+  def genArrayLit(a: ArrayLit) = {
+    val typ = a.typ
     val code = new ListBuffer[String]
+    a.exprList.foreach(genValueCode)
 
-    val list = exprList.map(genExpr)
-    code.appendAll(list.flatMap(_._2))
+    code.appendAll(a.exprList.flatMap(_.code))
 
     val temp = mkTemp()
-    code.append("param [0 : 2] = " + typ.typ.toString + ";")
-    code.append("param [1 : 2] = " + exprList.size + ";")
+    val eleSize = mkTemp()
+    code.append(eleSize + " = sizeof(" + typ.elementType.toString + ");")
+    code.append("param [0 : 2] = " + eleSize + ";")
+    code.append("param [1 : 2] = " + a.exprList.size + ";")
     code.append(temp + " = call newArray : 2" + ";")
 
     val assignCode =
-      for ((ele, i) <- list.map(_._1).zipWithIndex)
+      for ((ele, i) <- a.exprList.map(_.address).zipWithIndex)
       yield temp + "[" + i + "] = " + ele + ";"
     code.appendAll(assignCode)
-    (temp, code.toList)
+
+    a.address = temp
+    a.code = code.toList
   }
 
-  def genRecordLit(typ: RecordType, fieldLitList : List[FieldLit]): (String, List[String]) = {
+  def genRecordLit(r: RecordLit) {
+    val typ = r.typ
+    val fieldLitList = r.fieldLitList
     val temp = mkTemp()
     val code = new ListBuffer[String]
-    val list = fieldLitList.map(fieldLit => genExpr(fieldLit.expr))
-    code.appendAll(list.flatMap(_._2))
+    fieldLitList.foreach(genValueCode)
+    code.appendAll(fieldLitList.flatMap(_.code))
     code.append("param[0 : 1] =" + typ.toString + ";")
     code.append(temp + " = call newRecord : 1;")
     val assignCode =
-      for ((field, value) <- typ.fieldTypeList.map(_.fieldId.name).zip(list.map(_._1)))
+      for ((field, value) <- typ.fieldTypeList.map(_.fieldId.name).zip(fieldLitList.map(_.address)))
       yield temp + "." + field + " = " + value + ";"
     code.appendAll(assignCode)
-    (temp, code.toList)
+    r.address = temp
+    r.code = code.toList
   }
 
-  def funCall(paramList: List[String]): List[String] = {
-    val arity = paramList.size
-    paramList.zipWithIndex.map(p => "param[" + p._2 + " : " + arity + "] = " + p._1 )
+  def genFieldLit(f: FieldLit) {
+    genValueCode(f.expr)
+    f.address = f.expr.address
+    f.code = f.expr.code
   }
 
-  def genPrefixExpr(op: String, expr: Expression): (String, List[String]) = {
-    val (a, c) = genExpr(expr)
-    val temp = mkTemp()
-    (temp, c ::: List(temp + " = " + op + " " + a))
+  def genPrefixExpr(prefixExpr: PrefixExpr) {
+    genValueCode(prefixExpr.expr)
+    prefixExpr.address = prefixExpr.op + prefixExpr.expr.address
+    prefixExpr.code = prefixExpr.expr.code
   }
 
-  def genInfixExpr(op: String, left: Expression, right: Expression) = {
-    val (a1, c1) = genExpr(left)
-    val (a2, c2) = genExpr(right)
-    val temp = mkTemp()
-    (temp, c1 ::: c2 ::: List(temp + " = " + a1 + " " + op + " " + a2 ))
+  def genInfixExpr(i: InfixExpr) {
+    i.op match {
+      case op if arithmeticOp.contains(op) =>
+        genValueCode(i.left)
+        genValueCode(i.right)
+        i.address = mkTemp()
+        i.code = i.left.code ::: i.right.code :::
+          List(i.address + " = " + i.left.address + " " + i.op + " " + i.right.address + ";")
+      case _ =>
+        i.t = mkLabel()
+        i.f = mkLabel()
+        genJumpingCode(i)
+        val jumpingCode = i.code
+        i.address = mkTemp()
+        val code = new ListBuffer[String]
+        code.append(i.address + " = true;")
+        code.appendAll(jumpingCode)
+        code.append(i.f + ":")
+        code.append(i.address + " = false;")
+        code.append(i.t + ":")
+        i.code = code.toList
+    }
   }
 
-
-  def genCallExpr(callee: FunId, params: List[Expression]): (String, List[String]) = {
+  def genCallExpr(expr: CallExpr) {
+    val callee = expr.callee
+    val params = expr.paramList
     val arity = params.size
-    val list = params.map(genExpr)
+
+    params.foreach(genValueCode)
     val prepareParamCode =
-      for ((a, i) <- list.map(_._1).zipWithIndex)
-        yield "param[" + i + " : " + arity + "] = " + a + ";"
-    val callCode = "call " + callee.name + " : " + arity + ";"
-    ("", list.flatMap(_._2) ::: prepareParamCode ::: List(callCode))
+      for ((a, i) <- params.map(_.address).zipWithIndex)
+      yield "param[" + i + " : " + arity + "] = " + a + ";"
+    expr.address = "call " + callee.name + " : " + arity + ";"
+    expr.code = params.flatMap(_.code) ::: prepareParamCode;
   }
 
-  def genCastExpr(expr: Expression, typ: Type) = {
-    val (a, code) = genExpr(expr)
-    (a, code ::: List(a + ":" + typ.toString))
+  def genCastExpr(castExpr: CastExpr) {
+    genValueCode(castExpr.expr)
+    castExpr.code = castExpr.code
+    castExpr.address = castExpr.expr.address + " : " + castExpr.typ.toString
   }
 
-  def genFieldExpr(expr: Expression, field: String) = {
-    val (a, code) = genExpr(expr)
-    (a + "." + field, code)
+
+  def genFieldExpr(fieldExpr: FieldExpr) = {
+    genValueCode(fieldExpr.expr)
+    fieldExpr.address = fieldExpr.expr.address + "." + fieldExpr.fieldId.name
+    fieldExpr.code = fieldExpr.expr.code
   }
 }
