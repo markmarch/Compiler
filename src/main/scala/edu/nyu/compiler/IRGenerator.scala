572065a84dfb7573b540eeec885d12dbfcb21f74
mark_march <markofmarch@gmail.com>
2011-12-15 01:52:27 -0500

    fixed assignment bug where array write was seen as copy instruction

--- src/main/scala/edu/nyu/compiler/IRGenerator.scala
+++ src/main/scala/edu/nyu/compiler/IRGenerator.scala
@@ -1,6 +1,6 @@
 package edu.nyu.compiler
 
-import collection.mutable.{ListBuffer,  HashMap}
+import collection.mutable.{ListBuffer, HashMap}
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,59 +13,49 @@ trait IRGenerator extends TypeAnalyzer {
   private[this] val relationOp = Set("==", "!=", ">=", ">", "<=", "<")
   private[this] val arithmeticOp = Set("+", "-", "*", "/", "%")
 
-  def findAddresses(addresses : HashMap[String, Address], scope : Scope): HashMap[String, Address] = {
-    for(key <- scope.symbols.keySet) {
+  def findAddresses(addresses: HashMap[String, Address], scope: Scope): HashMap[String, Address] = {
+    for (key <- scope.symbols.keySet) {
       val s = scope.symbols(key)
       s.definition match {
-        case v : VarId => s.address = NameAddr(nextUniqueName(addresses, v.name))
-        case f : FieldId => s.address = NameAddr(nextUniqueName(addresses, f.name))
-        case f : FunId => s.address = NameAddr(nextUniqueName(addresses, f.name))
+        case v: VarId => s.address = NameAddr(nextUniqueName(addresses, v.name, s.typ), s.typ)
+        case f: FieldId => s.address = NameAddr(nextUniqueName(addresses, f.name, s.typ), s.typ)
+        case f: FunId => s.address = NameAddr(nextUniqueName(addresses, f.name, s.typ), s.typ)
       }
     }
-    for(s <- scope.children)
+    for (s <- scope.children)
       findAddresses(addresses, s)
     addresses
   }
 
-  private[this] def nextUniqueName(map : HashMap[String, Address], srcName : String) : String = {
+  private[this] def nextUniqueName(map: HashMap[String, Address], srcName: String, typ: Type): String = {
     val result = if (map.contains(srcName)) {
       var i = 0
-      while(map.contains(srcName + i))
+      while (map.contains(srcName + i))
         i += 1
       srcName + i
     } else srcName
-    map.put(result, NameAddr(result))
+    map.put(result, NameAddr(result, typ))
     result
   }
 
   private[this] def mkLabel() = {
     val fun = symbolTable.currnetFuction
-    nextUniqueName(fun.addresses, "L_" + fun.id.name)
+    nextUniqueName(fun.addresses, "L_" + fun.id.name, null)
   }
 
-  private[this] def mkTemp() = {
-    NameAddr(nextUniqueName(symbolTable.currnetFuction.addresses, "t"))
-  }
-  
-  def generateIR(p: Program): List[String] = {
-    analyzeType(p) match {
-      case Left(e) => println(e); Nil
-      case Right(_) => p.funList.flatMap(genFunDef)
-    }
+  private[this] def mkTemp(typ: Type) = {
+    NameAddr(nextUniqueName(symbolTable.currnetFuction.addresses, "t", typ), typ)
   }
 
-  def genFunDef(f: FunDef) = {
+  def genProgram(p: Program): ProgramIr = ProgramIr(p.funList.map(genFunDef))
+
+  def genFunDef(f: FunDef): FunIr = {
     symbolTable.push(f.scope)
     symbolTable.currnetFuction = f
-    f.addresses = findAddresses(HashMap.empty[String,  Address], f.scope)
+    f.addresses = findAddresses(HashMap.empty[String, Address], f.scope)
     genStmt(f.blockStmt)
     symbolTable.pop()
-    f.id.name + " = fun" + f.typ.fromType.toString + " -> " + f.typ.returnType.toString ::
-      f.blockStmt.code.map(
-        i => i match {
-          case _: EmptyLabel => "  " + i.toString
-          case _ => "  " + i.toString + ";"
-        })
+    FunIr(f.id.name, f.typ, f.blockStmt.code, f.addresses.size * 8)
   }
 
   def genStmt(stmt: Stmt) {
@@ -152,44 +142,31 @@ trait IRGenerator extends TypeAnalyzer {
   def genAssignStmt(a: AssignStmt) = genAssignment(a, a.left, a.right)
 
   def genAssignment(s: Stmt, left: Expression, right: Expression) {
-    left match {
-      case s: SubscriptExpr => genSubscriptExpr(s, true)
-      case _ => genValueCode(left)
-    }
-    right match {
-      case t@InfixExpr("+", _, _) if right.typ == PrimitiveType("string") =>
-        genStringAddExpr(t)
-        s.code = left.code ::: t.code :::
-          List(CopyInstr(left.address, t.address))
-      case InfixExpr(op, l, r) if arithmeticOp.contains(op) =>
-        genValueCode(l)
-        genValueCode(r)
-        s.code = left.code ::: l.code :::
-          r.code ::: List(InfixInstr(left.address, op, l.address, r.address))
-      case PrefixExpr("!", _) =>
+    genValueCode(left)
+    (left, right) match {
+      case (sub: SubscriptExpr, PrefixExpr("!", e)) =>
+        genValueCode(e)
         right.t = mkLabel()
         right.f = mkLabel()
         genJumpingCode(right)
-        s.code = CopyInstr(left.address, ConstAddr("true")) :: right.code :::
-          List(LabeledInstruction(right.f, CopyInstr(left.address, ConstAddr("false"))), EmptyLabel(right.t))
-      case PrefixExpr(op, e) =>
-        genValueCode(e)
-        s.code = left.code ::: e.code
-        left match {
-          case sub: SubscriptExpr =>
-            right.address = mkTemp()
-            s.code = s.code :::
-              List(PrefixInstr(right.address, op, e.address), CopyInstr(left.address, right.address))
-          case _ => s.code = s.code ::: List(PrefixInstr(left.address, op, e.address))
-        }
-      case c: CastExpr =>
-        genValueCode(c)
-        s.code = left.code ::: c.code :::
-          List(CopyInstr(left.address, c.address))
+        s.code = ArrayWriteInstr(sub.left.address.asInstanceOf[NameAddr], sub.right.address, ConstAddr("true", bool)) :: right.code :::
+          List(LabeledInstruction(right.f,
+            ArrayWriteInstr(sub.left.address.asInstanceOf[NameAddr], sub.right.address, ConstAddr("false", bool))), EmptyLabel(right.t))
+      case (sub: SubscriptExpr, _) =>
+        genValueCode(right)
+        s.address = left.address
+        s.code = left.code ::: right.code :::
+          List(ArrayWriteInstr(sub.left.address.asInstanceOf[NameAddr], sub.right.address, right.address))
+      case (_, PrefixExpr("!", e)) =>
+        right.t = mkLabel()
+        right.f = mkLabel()
+        genJumpingCode(right)
+        s.code = CopyInstr(left.address, ConstAddr("true", bool)) :: right.code :::
+          List(LabeledInstruction(right.f, CopyInstr(left.address, ConstAddr("false", bool))), EmptyLabel(right.t))
       case _ =>
         genValueCode(right)
-        s.code = left.code ::: right.code :::
-          List(CopyInstr(left.address, right.address))
+        s.address = left.address
+        s.code = left.code ::: right.code ::: List(CopyInstr(left.address, right.address))
     }
   }
 
@@ -198,18 +175,17 @@ trait IRGenerator extends TypeAnalyzer {
     genValueCode(f.expr)
     val code = new ListBuffer[Instruction]
     code.appendAll(f.expr.code)
-    val size = mkTemp()
-    val index = mkTemp()
-    code.append(ParamInstr(0, 1, f.expr.address))
-    code.append(CallInstr(Some(size), "size", 1))
-    code.append(CopyInstr(index, ConstAddr("0")))
+    val size = mkTemp(int)
+    val index = mkTemp(int)
+    code.append(CallInstr(List(ParamInstr(0, 1, f.expr.address)), Some(size), "size", 1))
+    code.append(CopyInstr(index, ConstAddr("0", int)))
     val begin = mkLabel()
     val after = mkLabel()
     code.append(LabeledInstruction(begin, RelopJumpInstr(">=", index, size, after)))
-    code.append(ArrayReadInstr(f.varId.address, f.expr.address, index))
+    code.append(ArrayReadInstr(f.varId.address.asInstanceOf[NameAddr], f.expr.address.asInstanceOf[NameAddr], index))
     f.blockStmt.stmtList.foreach(genStmt)
     code.appendAll(f.blockStmt.stmtList.flatMap(_.code))
-    code.append(InfixInstr(index, "+", index, ConstAddr("1")))
+    code.append(InfixInstr(index, "+", index, ConstAddr("1", int)))
     code.append(UnconditionalJumpInstr(begin))
     code.append(EmptyLabel(after))
     f.code = code.toList
@@ -260,10 +236,10 @@ trait IRGenerator extends TypeAnalyzer {
 
   def genValueCode(expr: Expression): Unit = expr match {
     case i: VarId => i.address = i.symbol.address
-    case b: BoolLit => b.address = ConstAddr(b.value.toString)
-    case s: StringLit => s.address = ConstAddr("\"" + s.value + "\"")
-    case i: IntLit => i.address = ConstAddr(i.value.toString)
-    case n: NullLit => n.address = ConstAddr("null")
+    case b: BoolLit => b.address = ConstAddr(b.value.toString, bool)
+    case s: StringLit => s.address = ConstAddr("\"" + s.value + "\"", string)
+    case i: IntLit => i.address = ConstAddr(i.value.toString, int)
+    case n: NullLit => n.address = ConstAddr("null", NullType())
     case a: ArrayLit => genArrayLit(a)
     case r: RecordLit => genRecordLit(r)
     case f: FieldLit => genFieldLit(f)
@@ -287,10 +263,11 @@ trait IRGenerator extends TypeAnalyzer {
     genValueCode(subExpr.right)
     subExpr.code = subExpr.left.code ::: subExpr.right.code
     if (returnRef) {
-      subExpr.address = NameAddr(subExpr.left.address + "[" + subExpr.right.address + "]")
+      subExpr.address = NameAddr(subExpr.left.address + "[" + subExpr.right.address + "]", subExpr.left.typ)
     } else {
-      subExpr.address = mkTemp()
-      subExpr.code = subExpr.code ::: List(ArrayReadInstr(subExpr.address, subExpr.left.address, subExpr.right.address))
+      subExpr.address = mkTemp(subExpr.left.typ.asInstanceOf[ArrayType].elementType)
+      subExpr.code = subExpr.code ::: List(ArrayReadInstr(subExpr.address.asInstanceOf[NameAddr],
+        subExpr.left.address.asInstanceOf[NameAddr], subExpr.right.address))
     }
   }
 
@@ -301,14 +278,14 @@ trait IRGenerator extends TypeAnalyzer {
 
     code.appendAll(a.exprList.flatMap(_.code))
 
-    val temp = mkTemp()
-    code.append(ParamInstr(0, 2, SizeOfAddr(typ.elementType)))
-    code.append(ParamInstr(1, 2, ConstAddr(a.exprList.size.toString)))
-    code.append(CallInstr(Some(temp), "newArray", 2))
+    val temp = mkTemp(a.typ)
+    val paramList = List(ParamInstr(0, 2, SizeOfAddr(typ.elementType)),
+      ParamInstr(1, 2, ConstAddr(a.exprList.size.toString, int)))
+    code.append(CallInstr(paramList, Some(temp), "newArray", 2))
 
     val assignCode =
-      for ((ele, i) <- a.exprList.map(_.address).zipWithIndex)
-      yield ArrayWriteInstr(temp, ConstAddr(i.toString), ele)
+      for ((ele, index) <- a.exprList.map(_.address).zipWithIndex)
+      yield ArrayWriteInstr(temp, ConstAddr(index.toString, int), ele)
     code.appendAll(assignCode)
 
     a.address = temp
@@ -318,12 +295,11 @@ trait IRGenerator extends TypeAnalyzer {
   def genRecordLit(r: RecordLit) {
     val typ = r.typ.asInstanceOf[RecordType]
     val fieldLitList = r.fieldLitList
-    val temp = mkTemp()
+    val temp = mkTemp(r.typ)
     val code = new ListBuffer[Instruction]
     fieldLitList.foreach(genValueCode)
     code.appendAll(fieldLitList.flatMap(_.code))
-    code.append(ParamInstr(0, 1, SizeOfAddr(typ)))
-    code.append(CallInstr(Some(temp), "newRecord", 1))
+    code.append(CallInstr(List(ParamInstr(0, 1, SizeOfAddr(typ))), Some(temp), "newRecord", 1))
     val assignCode =
       for ((field, value) <- typ.fieldTypeList.map(_.fieldId.name).zip(fieldLitList.map(_.address)))
       yield RecWriteInstr(temp, field, value)
@@ -340,8 +316,8 @@ trait IRGenerator extends TypeAnalyzer {
 
   def genPrefixExpr(p: PrefixExpr) {
     genValueCode(p.expr)
-    p.address = mkTemp()
-    p.code = p.expr.code ::: List(PrefixInstr(p.address, p.op, p.expr.address))
+    p.address = mkTemp(p.expr.typ)
+    p.code = p.expr.code ::: List(PrefixInstr(p.address.asInstanceOf[NameAddr], p.op, p.expr.address))
   }
 
   def genInfixExpr(i: InfixExpr) {
@@ -350,19 +326,19 @@ trait IRGenerator extends TypeAnalyzer {
       case op if arithmeticOp.contains(op) =>
         genValueCode(i.left)
         genValueCode(i.right)
-        i.address = mkTemp()
+        i.address = mkTemp(i.typ)
         i.code = i.left.code ::: i.right.code :::
-          List(InfixInstr(i.address, i.op, i.left.address, i.right.address))
+          List(InfixInstr(i.address.asInstanceOf[NameAddr], i.op, i.left.address, i.right.address))
       case _ =>
         i.t = mkLabel()
         i.f = mkLabel()
         genJumpingCode(i)
         val jumpingCode = i.code
-        i.address = mkTemp()
+        i.address = mkTemp(i.typ)
         val code = new ListBuffer[Instruction]
-        code.append(CopyInstr(i.address, ConstAddr("true")))
+        code.append(CopyInstr(i.address, ConstAddr("true", bool)))
         code.appendAll(jumpingCode)
-        code.append(LabeledInstruction(i.f, CopyInstr(i.address, ConstAddr("false"))))
+        code.append(LabeledInstruction(i.f, CopyInstr(i.address, ConstAddr("false", bool))))
         code.append(EmptyLabel(i.t))
         i.code = code.toList
     }
@@ -376,10 +352,9 @@ trait IRGenerator extends TypeAnalyzer {
     val code = new ListBuffer[Instruction]
     code.appendAll(left.code)
     code.appendAll(right.code)
-    code.append(ParamInstr(0, 2, i.left.address))
-    code.append(ParamInstr(1, 2, i.right.address))
-    i.address = mkTemp()
-    code.append(CallInstr(Some(i.address), "append", 2))
+    val paramList = List(ParamInstr(0, 2, i.left.address), ParamInstr(1, 2, i.right.address))
+    i.address = mkTemp(i.typ)
+    code.append(CallInstr(paramList, Some(i.address), "append", 2))
     i.code = code.toList
   }
 
@@ -392,26 +367,31 @@ trait IRGenerator extends TypeAnalyzer {
     val prepareParamCode =
       for ((a, i) <- params.map(_.address).zipWithIndex)
       yield ParamInstr(i, arity, a)
-    expr.code = params.flatMap(_.code) ::: prepareParamCode
+    expr.code = params.flatMap(_.code)
     val optionAddr = if (withReturnValue) None
     else {
-      expr.address = mkTemp()
+      expr.address = mkTemp(expr.typ)
       Some(expr.address)
     }
-    expr.code = expr.code ::: List(CallInstr(optionAddr, callee.name, arity))
+    expr.code = expr.code ::: List(CallInstr(prepareParamCode, optionAddr, callee.name, arity))
   }
 
   def genCastExpr(c: CastExpr) {
     genValueCode(c.expr)
-    c.address = mkTemp()
-    c.code = c.expr.code :::
-      List(CastInstr(c.address, c.expr.address, c.toType))
+    c.address = mkTemp(c.toType)
+    //    val cast = c.expr.typ match {
+    //      case t@PrimitiveType(from) if t != c.toType =>
+    //        List(CallInstr(List(ParamInstr(0, 1, c.expr.address)), Some(c.address), from + "2" + c.toType.toString, 1))
+    //      case _ => Nil
+    //    }
+    c.code = c.expr.code ::: List(CastInstr(c.address.asInstanceOf[NameAddr], c.expr.address, c.expr.typ))
   }
 
 
   def genFieldExpr(f: FieldExpr) = {
     genValueCode(f.expr)
-    f.address = mkTemp()
-    f.code = f.expr.code ::: List(RecReadInstr(f.address, f.expr.address, f.fieldId.name))
+    f.address = mkTemp(f.expr.typ)
+    f.code = f.expr.code ::: List(RecReadInstr(f.address.asInstanceOf[NameAddr],
+      f.expr.address.asInstanceOf[NameAddr], f.fieldId.name))
   }
 }
