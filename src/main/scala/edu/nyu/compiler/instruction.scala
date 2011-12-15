621dbbaf02317e8f7e2410e0bb43a0173f84055b
mark_march <markofmarch@gmail.com>
2011-12-15 01:50:15 -0500

    added type info to addresses

--- src/main/scala/edu/nyu/compiler/instruction.scala
+++ src/main/scala/edu/nyu/compiler/instruction.scala
@@ -4,21 +4,26 @@ sealed abstract class Instruction
 
 // top level
 case class ProgramIr(funList : List[FunIr]) {
+  def getStringRep : List[String] = funList.flatMap(
+    f => f.id + " = fun " + f.funType.toString :: f.stmtList.map(instr => instr match {
+      case e: EmptyLabel => e.toString
+      case i => "  " + i.toString + ";"
+    }))
   override def toString = funList.map(_.toString).mkString("\n")
 }
 
-case class FunIr(id : String, funType: FunType, stmtList: List[Instruction]) {
+case class FunIr(id : String, funType: FunType, stmtList: List[Instruction], frameSize: Int) {
   override def toString = id + " = fun " + funType.toString +"\n" + stmtList.map(_.toString + ";").mkString("\n")
 }
 
 // Address
 sealed  abstract class Address
 
-case class NameAddr(id : String) extends Address{
-  override def toString = id
+case class NameAddr(name : String, typ : Type) extends Address{
+  override def toString = name
 }
 
-case class ConstAddr(lit : String) extends Address{
+case class ConstAddr(lit : String, typ : Type) extends Address{
   override def toString = lit
 }
 
@@ -39,16 +44,16 @@ case class CopyInstr(left: Address, right: Address) extends Instruction {
   override def toString = left.toString + " = " + right.toString
 }
 
-case class InfixInstr(address : Address, op: String, left:Address, right:Address) extends Instruction{
+case class InfixInstr(address : NameAddr, op: String, left:Address, right:Address) extends Instruction{
   override def toString = address.toString + " = " + left.toString + " " + op + " " + right.toString
 }
 
-case class PrefixInstr(address : Address,  op: String, operand: Address) extends Instruction {
+case class PrefixInstr(address : NameAddr,  op: String, operand: Address) extends Instruction {
   override def toString = address.toString + " = " + op + operand.toString
 }
 
-case class CastInstr(address:Address, from: Address, typ: Type) extends Instruction {
-  override def toString = address.toString + " = " + from.toString + ":" + typ.toString
+case class CastInstr(address:NameAddr, from: Address, typ: Type) extends Instruction {
+  override def toString = address.toString + " = " + from.toString + ":" + address.typ.toString
 }
 
 case class UnconditionalJumpInstr(label : String) extends Instruction {
@@ -71,26 +76,27 @@ case class ParamInstr(index: Int,  arity: Int,  address: Address) extends Instru
   override def toString = "param [" + index + ":" + arity + "] = " + address.toString  
 }
 
-case class CallInstr(oa : Option[Address], funId : String,  arity: Int) extends Instruction {
-  override def toString = oa.map(_.toString + " = ").getOrElse("") + "call " + funId + " : " + arity
+case class CallInstr(paramList : List[ParamInstr], oa : Option[Address], funId : String,  arity: Int) extends Instruction {
+  override def toString = paramList.map(_.toString + ";").mkString("", "\n  ", "\n  ") +
+    oa.map(_.toString + " = ").getOrElse("") + "call " + funId + " : " + arity
 }
 
 case class ReturnInstr(oa : Option[Address]) extends Instruction {
   override def toString = "return" + oa.map(" " + _.toString).getOrElse("")
 }
 
-case class ArrayReadInstr(address: Address, array: Address, index: Address) extends Instruction {
+case class ArrayReadInstr(address: NameAddr, array: NameAddr, index: Address) extends Instruction {
   override def toString = address.toString + " = " + array.toString + "[" + index.toString + "]"
 }
 
-case class ArrayWriteInstr(array:Address, index: Address,  address: Address) extends Instruction {
+case class ArrayWriteInstr(array:NameAddr, index: Address,  address: Address) extends Instruction {
   override def toString = array.toString + "[" + index.toString + "] = " + address.toString
 }
 
-case class RecReadInstr(address: Address, record: Address, fieldId: String) extends Instruction {
+case class RecReadInstr(address: NameAddr, record: NameAddr, fieldId: String) extends Instruction {
   override def toString = address.toString + " = " + record.toString + "." + fieldId
 }
 
-case class RecWriteInstr(record: Address, fieldId: String, address: Address) extends Instruction {
+case class RecWriteInstr(record: NameAddr, fieldId: String, address: Address) extends Instruction {
   override def toString = record.toString + "." + fieldId + " = " + address.toString
 }
