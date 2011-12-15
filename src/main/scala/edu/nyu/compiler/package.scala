d00548f058598da7b06f2da987712113f78dbe2b
mark_march <markofmarch@gmail.com>
2011-12-15 01:42:01 -0500

    added package object to hold global vals

---
+++ src/main/scala/edu/nyu/compiler/package.scala
@@ -0,0 +1,14 @@
+package edu.nyu
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: xmj
+ * Date: 12/12/11
+ * Time: 3:25 PM
+ */
+
+package object compiler {
+  val int = PrimitiveType("int")
+  val string = PrimitiveType("string")
+  val bool = PrimitiveType("bool")
+}
